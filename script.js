5ed4e79ab088b56eb5f72d25d103bafe4ea86b1b
Annie Tran <tranthilananh105@gmail.com>
2023-10-30 15:45:30 +0200

    loading lazy images

--- script.js
+++ script.js
@@ -155,3 +155,22 @@ allSection.forEach(function(section){
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+//Loading lazy images
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+const loadImg = function (entries,observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if(!entry.isIntersecting) return;
+  //Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.classList.remove('lazy-img');
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root:null,
+  threshold: 0
+})
+imgTargets.forEach(img => imgObserver.observe(img));
